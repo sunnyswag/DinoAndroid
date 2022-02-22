89d5c2f79bb78ab24fcf3d7d50f70b1720b7a5af
sunnyswag <hhq126152@gmail.com>
2022-02-22 21:55:27 +0800

    完成两个主界面
    去除 viewBinding 和 dataBinding
    实现界面间的跳转

--- src/app/src/main/java/com/demo/dinoandroid/view/MainActivity.kt
+++ src/app/src/main/java/com/demo/dinoandroid/view/MainActivity.kt
@@ -3,21 +3,18 @@ package com.demo.dinoandroid.view
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import androidx.databinding.DataBindingUtil
 import com.demo.dinoandroid.R
-import com.demo.dinoandroid.databinding.ActivityMainBinding
+import pl.droidsonroids.gif.GifImageView
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // https://developer.android.com/topic/libraries/data-binding/expressions
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.dinoIcon.visibility = View.VISIBLE
-        binding.dinoIcon.setOnClickListener {
+        setContentView(R.layout.activity_main)
+        val dinoIcon = findViewById<GifImageView>(R.id.dino_icon)
+        dinoIcon.setOnClickListener {
             val intent = Intent(this, GameActivity::class.java)
             startActivity(intent)
         }
