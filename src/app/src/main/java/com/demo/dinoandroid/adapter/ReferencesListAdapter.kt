928ce036b78f51fd63e69e05deac6f7ee2287b8a
sunnyswag <hhq126152@gmail.com>
2022-02-23 10:01:30 +0800

    在主界面添加 RecyclerView 来展示 引用列表

---
+++ src/app/src/main/java/com/demo/dinoandroid/adapter/ReferencesListAdapter.kt
@@ -0,0 +1,4 @@
+package com.demo.dinoandroid.adapter
+
+class ReferencesListAdapter {
+}
--- src/app/src/main/java/com/demo/dinoandroid/view/MainActivity.kt
+++ src/app/src/main/java/com/demo/dinoandroid/view/MainActivity.kt
@@ -3,20 +3,31 @@ package com.demo.dinoandroid.view
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 import com.demo.dinoandroid.R
 import pl.droidsonroids.gif.GifImageView
 
 class MainActivity : AppCompatActivity() {
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // https://developer.android.com/topic/libraries/data-binding/expressions
         setContentView(R.layout.activity_main)
         val dinoIcon = findViewById<GifImageView>(R.id.dino_icon)
+        val references = findViewById<TextView>(R.id.references)
+        val referencesList = findViewById<RecyclerView>(R.id.referencesList)
+
         dinoIcon.setOnClickListener {
             val intent = Intent(this, GameActivity::class.java)
             startActivity(intent)
         }
+
+        references.setOnClickListener {
+            referencesList.visibility = View.VISIBLE
+        }
     }
+
+
 }
